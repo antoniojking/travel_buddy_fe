c61aabf5ef287b37a0f9d3c03f0891704c914f95
Elliot Olbright <elliotolbright@gmail.com>
2021-09-15 21:24:11 -0600

    set up explore page

--- spec/features/np_explore/index_spec.rb
+++ spec/features/np_explore/index_spec.rb
@@ -8,4 +8,21 @@ RSpec.describe 'Explore Page' do
     
     expect(current_path).to eq(explore_index_path)
   end 
+
+  it 'has a drop down menu' do 
+    select "Colorado", :from => "state"
+
+    click_button "Search State"
+
+    expect(current_path).to eq(explore_index_path)
+    expect(page).to have_content('NP Explore Page')
+
+  end
+
+  it 'has a search for activities form' do
+    fill_in :activity, with: "squirrels"
+
+    click_on 'Search' 
+    expect(current_path).to eq(explore_index_path)
+  end
 end
