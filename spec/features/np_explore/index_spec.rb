3b6036277b25821ea897e2b5c0c9372a2c362cb1
Elliot Olbright <elliotolbright@gmail.com>
2021-09-16 06:34:30 -0600

    dual dropdown menus passing rspec

--- spec/features/np_explore/index_spec.rb
+++ spec/features/np_explore/index_spec.rb
@@ -10,28 +10,35 @@ RSpec.describe 'Explore Page' do
   end 
 
   it 'has a drop down menu' do 
-    select "Colorado", :from => "state"
+    within "#option1" do
+      select "Colorado", :from => "state"
 
-    click_button "Search State"
-
-    expect(current_path).to eq(explore_index_path)
+      click_button "Search State"
+  
+      expect(current_path).to eq(explore_index_path)
+    end
     expect(page).to have_content('NP Explore Page')
-
   end
 
   it 'has a search for activities form' do
-    fill_in :activity, with: "squirrels"
+    within "#option1" do
+      fill_in :activity, with: "squirrels"
 
-    click_on 'Search' 
-    expect(current_path).to eq(explore_index_path)
+      click_on 'Search' 
+      expect(current_path).to eq(explore_index_path)
+    end
+   
   end
 
   it 'can add multiple params to url' do
-    fill_in :activity, with: "Hiking"
-    fill_in :state, with: "Colorado"
+    within "#option2" do 
+      select "Colorado", :from => "state"
+      select "Hiking", :from => "activity"
 
-    click_on 'Filter Results' 
-    expect(current_path).to eq(explore_index_path)
-    expect(page).to have_content("Searching in: CO, with activity Hiking")
+
+      click_on 'Filter Results' 
+      expect(current_path).to eq(explore_index_path)
+      expect(page).to have_content("Searching in: CO, with activity Hiking")
+    end
   end
 end
