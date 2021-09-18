74bd0ee02d67cabd4ea27c2efd47020b4086bd00
Matt Toensing <matthew.toensing@gmail.com>
2021-09-18 14:01:01 -0700

    Shralper contributing

--- spec/facades/nps_facade_spec.rb
+++ spec/facades/nps_facade_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe NationalParkFacade do
+
   describe "class methods" do
     describe ".all_parks" do
       it "returns an array of all the parks as park objects" do
-        parks = NationalParkFacade.all_parks
+        parks = Na tionalParkFacade.all_parks
 
         expect(parks).to be_an Array
         expect(parks.first).to be_a Park
