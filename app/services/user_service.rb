c5a13d9cbd788a6b6c611be7b61a37a6ac01906d
Michael Abbott <michael@mjabbottdesign.com>
2021-09-19 14:01:29 -0700

    Refactor user service params nested 'user'

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -6,10 +6,10 @@ class UserService
 
     def get_user_info(spotify_id, email, token, refresh_token)
       response = conn('https://travel-buddy-api.herokuapp.com/api/v1/users').post do |f|
-        f.params['spotify_id'] = spotify_id
-        f.params['email'] = email
-        f.params['token'] = token
-        f.params['refresh_token'] = refresh_token
+        f.params['user']['spotify_id'] = spotify_id
+        f.params['user']['email'] = email
+        f.params['user']['token'] = token
+        f.params['user']['refresh_token'] = refresh_token
       end
       JSON.parse(response.body, symbolize_names: true)
     end
