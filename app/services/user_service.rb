5e93026ab1b7e16f791b64dcefb7f7a4f80ddc3c
Michael Abbott <michael@mjabbottdesign.com>
2021-09-19 14:47:52 -0700

    Refactor remove user nested key

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -19,10 +19,10 @@ class UserService
     def get_user_info(spotify_id, email, token, refresh_token)
       response = conn.post("/api/v1/users") do |req|
         req.headers['CONTENT_TYPE'] = 'application/json'
-        req.params['user']['spotify_id'] = spotify_id
-        req.params['user']['email'] = email
-        req.params['user']['token'] = token
-        req.params['user']['refresh_token'] = refresh_token
+        req.params['spotify_id'] = spotify_id
+        req.params['email'] = email
+        req.params['token'] = token
+        req.params['refresh_token'] = refresh_token
       end
 
       JSON.parse(response.body, symbolize_names: true)
