65e09937eb1ec61d40f9b2b6edd2dd093df68f73
Matt Toensing <matthew.toensing@gmail.com>
2021-09-21 16:52:05 -0700

    Add tests and features to user dashboard

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -2,33 +2,34 @@ require 'rails_helper'
 
 RSpec.describe 'User dashboard page' do
   describe 'as an authenticated user' do
-    # let(:email) { 'funbucket13' }
-    # let(:password) { 'test' }
-    # let(:user) { User.create(email: email, password: password) }
 
-    # before do
-    #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-    # end
+    before(:each) do
+      json_response = File.read('spec/fixtures/user/user_info.json')
+      stub_request(:get, "https://travel-buddy-api.herokuapp.com/api/v1/users/3112").to_return(status: 200, body: json_response, headers: {})
+
+      json_response2 = File.read('spec/fixtures/trips/dashboard/trip_new.json')
+      stub_request(:get, "https://travel-buddy-api.herokuapp.com/api/v1/trips/2064").to_return(status: 200, body: json_response2, headers: {})
+
+      json_response3 = File.read('spec/fixtures/trips/dashboard/trip_new.json')
+      stub_request(:get, "https://travel-buddy-api.herokuapp.com/api/v1/trips/2065").to_return(status: 200, body: json_response3, headers: {})
+
+      json_response4 = File.read('spec/fixtures/trips/dashboard/trip_new.json')
+
+      stub_request(:get, "https://travel-buddy-api.herokuapp.com/api/v1/trips/2066").to_return(status: 200, body: json_response4, headers: {})
+
+      @user = UserFacade.current_user_info(3112)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+      visit '/user_dashboard'
+    end
 
     describe 'when I visit the "/user_dashboard" path' do
-      before { visit '/user_dashboard' }
 
       it 'displays welcome message' do
         expect(current_path).to eq('/user_dashboard')
-        expect(page).to have_content("Welcome User!")
+        expect(page).to have_content("Welcome #{@user.email}!")
       end
-
-      it 'displays a link to read trip notifications/messages' do
-        expect(page).to have_link("Notifications")
-      end
-
-      # context 'when I click on the notifications link' do
-      #   before { click_link('Notifications') }
-      #
-      #   it 'redirects me to the notifications page' do
-      #     expect(current_path).to eq('/notifications')
-      #   end
-      # end
 
       it 'displays a button to explore national parks' do
         expect(page).to have_button("Explore National Parks")
@@ -45,36 +46,57 @@ RSpec.describe 'User dashboard page' do
       it 'displays a current trip section' do
         within "#current_trip" do
           expect(page).to have_content("Current Trip")
-          # expect(page).to have_content()
-          # expect(page).to have_field()
-          # expect(page).to have_button()
+          @user.current_trips.each do |trip|
+            expect(page).to have_link(trip[:name])
+          end
+
+          within("#current-trip-#{@user.current_trips[0][:id]}") do
+            click_link @user.current_trips[0][:name]
+          end
+
+          expect(current_path).to eq("/trips/dashboard/#{@user.current_trips[0][:id]}")
         end
       end
 
       it 'displays an upcoming trips section' do
         within "#upcoming_trips" do
           expect(page).to have_content("Upcoming Trips")
-          # expect(page).to have_content()
-          # expect(page).to have_field()
-          # expect(page).to have_button()
+
+          @user.upcoming_trips.each do |trip|
+            expect(page).to have_link(trip[:name])
+          end
+
+          within("#upcoming-trip-#{@user.upcoming_trips[0][:id]}") do
+            click_link @user.upcoming_trips[0][:name]
+          end
+
+          expect(current_path).to eq("/trips/dashboard/#{@user.upcoming_trips[0][:id]}")
         end
       end
 
       it 'displays a past trips section' do
         within "#past_trips" do
           expect(page).to have_content("Past Trips")
-          # expect(page).to have_content()
-          # expect(page).to have_field()
-          # expect(page).to have_button()
+
+          @user.past_trips.each do |trip|
+            expect(page).to have_link(trip[:name])
+          end
+
+          within("#past-trip-#{@user.past_trips[0][:id]}") do
+            click_link @user.past_trips[0][:name]
+          end
+
+          expect(current_path).to eq("/trips/dashboard/#{@user.past_trips[0][:id]}")
         end
       end
 
-      it 'displays a travel_buddies section' do
-        within "#travel_buddies" do
-          expect(page).to have_content("Travel Buddies")
-          # expect(page).to have_content()
-          # expect(page).to have_field()
-          # expect(page).to have_button()
+      it 'displays a friends section' do
+        within "#friends" do
+          expect(page).to have_content("Friends")
+          
+          @user.friends.each do |friend|
+            expect(page).to have_content(friend[:email])
+          end
         end
       end
     end
