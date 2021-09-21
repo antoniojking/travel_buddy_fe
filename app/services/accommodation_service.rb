77e576b64e9fcd6e65be4fe3277fa644d4fab75a
Carina Sweet <carina.sweet06@gmail.com>
2021-09-20 21:37:23 -0600

    Add accommodations service

--- app/services/accommodation_service.rb
+++ app/services/accommodation_service.rb
@@ -1,18 +1,18 @@
 class AccommodationService < BaseService
-  def self.get_trip_accommodations(trip_id)
-    response = conn.get("/api/v1/trip/#{trip_id}/accommodations")
-
-    JSON.parse(response.body, symbolize_names: true)
-  end
+  # def self.get_trip_accommodations(trip_id)
+  #   response = conn.get("/api/v1/trips/#{trip_id}/accommodations")
+  #
+  #   JSON.parse(response.body, symbolize_names: true)
+  # end
 
   def self.get_accommodation(trip_id, accommodation_id)
-    response = conn.get("/api/v1/trip/#{trip_id}/accommodations/#{accommodation_id}")
+    response = conn.get("/api/v1/trips/#{trip_id}/accommodations/#{accommodation_id}")
 
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def self.create_trip_accommodation(trip_id, name, location, description)
-    response = conn.post("/api/v1/trip/#{trip_id}/accommodations") do |req|
+  def self.create_trip_accommodations(trip_id, name, location, description)
+    response = conn.post("/api/v1/trips/#{trip_id}/accommodations") do |req|
       req.headers['CONTENT_TYPE'] = 'application/json'
       req.params['name'] = name
       req.params['location'] = location
@@ -23,11 +23,11 @@ class AccommodationService < BaseService
   end
 
   def self.delete_trip_accommodations(trip_id, accommodation_id)
-    conn.delete("/api/v1/trip/#{trip_id}/accommodations/#{accommodation_id}")
+    conn.delete("/api/v1/trips/#{trip_id}/accommodations/#{accommodation_id}")
   end
 
   def self.update_trip_accommodations(trip_id, accommodation_id, name, location, description)
-    response = conn.put("/api/v1/trip/#{trip_id}/accommodations/#{accommodation_id}") do |req|
+    response = conn.put("/api/v1/trips/#{trip_id}/accommodations/#{accommodation_id}") do |req|
       req.headers['CONTENT_TYPE'] = 'application/json'
       req.params['name'] = name
       req.params['location'] = location
