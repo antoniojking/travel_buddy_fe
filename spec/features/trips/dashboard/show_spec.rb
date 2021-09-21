864725c71d73e16aab2d59ef770f35534b50d2ff
Michael Abbott <michael@mjabbottdesign.com>
2021-09-20 17:57:54 -0700

    Refactor trip dashboard show with 'before each'

--- spec/features/trips/dashboard/show_spec.rb
+++ spec/features/trips/dashboard/show_spec.rb
@@ -5,24 +5,22 @@ RSpec.describe 'Trips dashboard page' do
     # before do
     #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     # end
-    # before :each do
-    #   visit trips_dashboard_path(6)
-    # end
+    before :each do
+      json_response = File.read('spec/fixtures/trips/dashboard/trip_new.json')
+      stub_request(:get, "https://travel-buddy-api.herokuapp.com/api/v1/trips/6").
+      with(
+        headers: {
+       'Accept'=>'*/*',
+       'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+       'User-Agent'=>'Faraday v1.8.0'
+        }).
+      to_return(status: 200, body: json_response, headers: {})
+
+      visit trips_dashboard_path(6)
+    end
 
     describe "when I visit a trip's dashboard" do
       it 'displays trip attributes' do
-        json_response = File.read('spec/fixtures/trips/dashboard/trip_new.json')
-        stub_request(:get, "https://travel-buddy-api.herokuapp.com/api/v1/trips/6").
-        with(
-          headers: {
-         'Accept'=>'*/*',
-         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-         'User-Agent'=>'Faraday v1.8.0'
-          }).
-        to_return(status: 200, body: json_response, headers: {})
-
-        visit trips_dashboard_path(6)
-
         expect(current_path).to eq(trips_dashboard_path(6))
         expect(page).to have_content('Trip Name')
         expect(page).to have_content('National Park Name')
@@ -31,7 +29,7 @@ RSpec.describe 'Trips dashboard page' do
 
       describe 'Weather' do
         #call on weather api facade
-        xit 'displays a weather forecast section' do
+        it 'displays a weather forecast section' do
           within "#weather" do
             expect(page).to have_content('Weather Forecast')
             # expect(page).to have_content()
@@ -42,7 +40,7 @@ RSpec.describe 'Trips dashboard page' do
       end
 
       describe 'Accommodations' do
-        xit 'displays an accommodations section' do
+        it 'displays an accommodations section' do
           within "#accommodations" do
             expect(page).to have_content('Accommodations')
             # expect(page).to have_content()
@@ -54,19 +52,6 @@ RSpec.describe 'Trips dashboard page' do
 
       describe 'Trip Checklists' do
         it 'displays a trip checklists section' do
-          #before each stub
-          json_response = File.read('spec/fixtures/trips/dashboard/trip_new.json')
-          stub_request(:get, "https://travel-buddy-api.herokuapp.com/api/v1/trips/6").
-          with(
-            headers: {
-           'Accept'=>'*/*',
-           'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-           'User-Agent'=>'Faraday v1.8.0'
-            }).
-          to_return(status: 200, body: json_response, headers: {})
-
-          visit trips_dashboard_path(6)
-
           within "#checklists" do
             expect(page).to have_content('Trip Checklist(s)')
             expect(page).not_to have_selector("#checklist-1")
@@ -77,19 +62,6 @@ RSpec.describe 'Trips dashboard page' do
         end
 
         it 'has a form to create new checklists' do
-          #before each stub
-          json_response = File.read('spec/fixtures/trips/dashboard/trip_new.json')
-          stub_request(:get, "https://travel-buddy-api.herokuapp.com/api/v1/trips/6").
-          with(
-            headers: {
-           'Accept'=>'*/*',
-           'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-           'User-Agent'=>'Faraday v1.8.0'
-            }).
-          to_return(status: 200, body: json_response, headers: {})
-
-          visit trips_dashboard_path(6)
-
           within "#checklists" do
             expect(page).to have_field(:category)
             expect(page).to have_button('Create Checklist')
@@ -98,18 +70,6 @@ RSpec.describe 'Trips dashboard page' do
 
         context 'when I fill in the checklist name field and click on the create checklist button' do
           it 'adds the created checklist to the list as a link' do
-            json_response = File.read('spec/fixtures/trips/dashboard/trip_new.json')
-            stub_request(:get, "https://travel-buddy-api.herokuapp.com/api/v1/trips/6").
-            with(
-              headers: {
-             'Accept'=>'*/*',
-             'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-             'User-Agent'=>'Faraday v1.8.0'
-              }).
-            to_return(status: 200, body: json_response, headers: {})
-
-            visit trips_dashboard_path(6)
-
             stub_request(:post, "https://travel-buddy-api.herokuapp.com/api/v1/trips/6/checklists").
             with(
                body: "{\"category\":\"Personal Luggage\",\"trip_id\":\"6\"}",
@@ -169,7 +129,7 @@ RSpec.describe 'Trips dashboard page' do
       end
 
       describe 'Joining Freinds' do
-        xit 'displays a friends joining section' do
+        it 'displays a friends joining section' do
           within "#friends_joining" do
             expect(page).to have_content('Friends Joining')
             # expect(page).to have_content()
