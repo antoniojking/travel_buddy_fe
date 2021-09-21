92cca5fa8e198f6f018a7b5f6da785a8a454749b
Carina Sweet <carina.sweet06@gmail.com>
2021-09-21 16:59:40 -0600

    Update user facade and test

--- app/facades/user_facade.rb
+++ app/facades/user_facade.rb
@@ -1,5 +1,11 @@
 class UserFacade
   class << self
+    def current_user_info(user_id)
+      json = UserService.get_user_info(user_id)
+
+      CurrentUser.new(json[:data])
+    end
+
     def create_user(spotify_id, email, token, refresh_token)
       json = UserService.find_or_create_user(spotify_id, email, token, refresh_token)
       User.new(json)
---
+++ spec/facades/user_facade_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe UserFacade do
+  it 'can create current user based off of user id' do
+    json_response = File.read('spec/fixtures/user/user_info.json')
+    stub_request(:get, 'https://travel-buddy-api.herokuapp.com/api/v1/users/4').to_return(status: 200, body: json_response)
+
+    user = UserFacade.current_user_info(4)
+
+    expect(user.id).to eq('3112')
+    expect(user.email).to eq('amee@haag-homenick.net')
+    expect(user.current_trips).to be_a(Array)
+    expect(user.current_trips.count).to eq(1)
+    expect(user.upcoming_trips).to be_a(Array)
+    expect(user.upcoming_trips.count).to eq(2)
+    expect(user.past_trips).to be_a(Array)
+    expect(user.past_trips.count).to eq(1)
+    expect(user.friends).to be_a(Array)
+    expect(user.friends.count).to eq(1)
+  end
+end
