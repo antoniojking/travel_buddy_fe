349be9883dd41159ec944c5b7303662056e23111
Michael Abbott <michael@mjabbottdesign.com>
2021-09-21 16:30:01 -0700

    Hide method that is not in use

--- app/facades/checklist_facade.rb
+++ app/facades/checklist_facade.rb
@@ -1,11 +1,11 @@
 class ChecklistFacade
   class << self
-    def by_trip(trip_id)
-      json = ChecklistService.by_trip(trip_id)
-      json[:data].map do |checklist|
-        Checklist.new(checklist)
-      end
-    end
+    # def by_trip(trip_id)
+    #   json = ChecklistService.by_trip(trip_id)
+    #   json[:data].map do |checklist|
+    #     Checklist.new(checklist)
+    #   end
+    # end
 
     def trip_checklist_new(trip_id, category)
       ChecklistService.trip_checklist_new(trip_id, category)
