89b287ee27c8e359a92af2cb654163aa2b814b18
Carina Sweet <carina.sweet06@gmail.com>
2021-09-21 16:36:15 -0600

    Add current user poro and test

---
+++ app/poros/current_user.rb
@@ -0,0 +1,17 @@
+class CurrentUser
+  attr_reader :id,
+              :email,
+              :current_trips,
+              :upcoming_trips,
+              :past_trips,
+              :friends
+
+  def initialize(current_user_info)
+    @id =             current_user_info[:id]
+    @email =          current_user_info[:email]
+    @current_trips =  current_user_info[:attributes][:current_trips]
+    @upcoming_trips = current_user_info[:attributes][:upcoming_trips]
+    @past_trips =     current_user_info[:attributes][:past_trips]
+    @friends =        current_user_info[:attributes][:friends]
+  end
+end
