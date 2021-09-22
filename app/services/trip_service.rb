8ef6ab02b431038bb7021c3913373f5683ff7ae1
Michael Abbott <michael@mjabbottdesign.com>
2021-09-22 14:29:26 -0700

    Add #patch_trip to TripService

--- app/services/trip_service.rb
+++ app/services/trip_service.rb
@@ -4,6 +4,13 @@ class TripService
       response = conn.get("/api/v1/trips/#{trip_id}")
       JSON.parse(response.body, symbolize_names: true)
     end
+
+    def patch_trip(trip_id, update_info)
+      conn.patch("/api/v1/trips/#{trip_id}") do |req|
+        req.params['start_date'] = update_info[:start_date] unless update_info[:start_date].empty?
+        req.params['end_date'] = update_info[:end_date] unless update_info[:end_date].empty?
+      end
+    end
   end
 
   def self.conn
