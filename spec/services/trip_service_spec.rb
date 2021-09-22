e40ea4b520349b221c07e9ad5fdfb5e569b20d13
Elliot Olbright <elliotolbright@gmail.com>
2021-09-22 14:55:53 -0600

    create ::get_trip_details service test

--- spec/services/trip_service_spec.rb
+++ spec/services/trip_service_spec.rb
@@ -27,5 +27,36 @@ describe TripService do
         expect(trip[:data][:attributes][:park_name]).to be_a(String)
       end
     end
+
+    context '::get_trip_details' do
+      it 'can get a trips details' do
+        json_response = File.read('spec/fixtures/trip_details.json')
+        stub_request(:get, "https://travel-buddy-api.herokuapp.com/api/v1/trips/7").
+         with(
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent'=>'Faraday v1.8.0'
+           }).
+         to_return(status: 200, body: json_response, headers: {})
+
+        trip = TripService.get_trip_details(7)
+
+        expect(trip).to be_a Hash
+        expect(trip).to have_key(:data)
+
+        expect(trip[:data][:attributes]).to have_key(:name)
+        expect(trip[:data][:attributes][:name]).to be_a(String)
+
+        expect(trip[:data][:attributes]).to have_key(:park_code)
+        expect(trip[:data][:attributes][:park_code]).to be_a(String)
+
+        expect(trip[:data][:attributes]).to have_key(:user_id)
+        expect(trip[:data][:attributes][:user_id]).to be_a(Integer)
+
+        expect(trip[:data][:attributes][:travel_buddies][0]).to have_key(:id)
+        expect(trip[:data][:attributes][:travel_buddies][0][:id]).to be_a(Integer)
+      end
+    end 
   end 
 end 
