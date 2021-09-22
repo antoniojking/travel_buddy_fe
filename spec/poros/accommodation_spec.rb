1a9dbf68d096887ef789d6d4e885f4072852d877
Carina Sweet <carina.sweet06@gmail.com>
2021-09-21 19:39:22 -0600

    Add id to accommodations poro

--- spec/poros/accommodation_spec.rb
+++ spec/poros/accommodation_spec.rb
@@ -3,16 +3,19 @@ require 'rails_helper'
 RSpec.describe Accommodation do
   it 'exists and has attributes' do
     attributes = {
-      name: 'Jenny Lake Campground',
-      location: 'Teton Park Rd, Moose, WY 83012',
-      details: 'We will be staying at Jenny Lake site 15 from 11/18 to 11/25.'
+      id: 21,
+      attributes:
+        {name: 'Jenny Lake Campground',
+        location: 'Teton Park Rd, Moose, WY 83012',
+        details: 'We will be staying at Jenny Lake site 15 from 11/18 to 11/25.'}
     }
 
     accommodation = Accommodation.new(attributes)
 
     expect(accommodation).to be_an_instance_of(Accommodation)
-    expect(accommodation.name).to eq(attributes[:name])
-    expect(accommodation.location).to eq(attributes[:location])
-    expect(accommodation.details).to eq(attributes[:details])
+    expect(accommodation.id).to eq(attributes[:id])
+    expect(accommodation.name).to eq(attributes[:attributes][:name])
+    expect(accommodation.location).to eq(attributes[:attributes][:location])
+    expect(accommodation.details).to eq(attributes[:attributes][:details])
   end
 end
