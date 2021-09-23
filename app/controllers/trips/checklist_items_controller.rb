8c4fea025e3d6d18d70697c02863686d261fc298
Matt Kragen <mattdkragen@gmail.com>
2021-09-22 17:31:15 -0700

    Implement #create for ChecklistItem

--- app/controllers/trips/checklist_items_controller.rb
+++ app/controllers/trips/checklist_items_controller.rb
@@ -1,4 +1,9 @@
 class Trips::ChecklistItemsController < ApplicationController
+  def create
+    ChecklistItemFacade.new_item(params[:dashboard_id], params[:checklist_id], params[:name], current_user.id.to_i)
+    redirect_to "/trips/dashboard/#{params[:dashboard_id]}/checklist/#{params[:checklist_id]}"
+  end
+  
   def update
     item_id = params[:id]
     ChecklistItemFacade.edit_item(params[:dashboard_id], params[:checklist_id], item_id, params[:name])
--- spec/features/checklists/show_spec.rb
+++ spec/features/checklists/show_spec.rb
@@ -39,7 +39,7 @@ RSpec.describe "Checklist show page" do
       updated_json_response = File.read('spec/fixtures/updated_checklist_show.json')
       
       stub_request(:patch, "https://travel-buddy-api.herokuapp.com/api/v1/trips/8/checklists/2/checklist_items/1").
-         with(body: {"item_id"=>"1", "name"=>"Cheese Doodles"}).
+         with(body: { "item_id"=>"1", "name"=>"Cheese Doodles" }).
             to_return(status: 200, body: updated_json_response)
 
       stub_request(:get, "https://travel-buddy-api.herokuapp.com/api/v1/trips/#{@trip_id}/checklists/#{@checklist_id}").
@@ -72,6 +72,29 @@ RSpec.describe "Checklist show page" do
 
       expect(current_path).to eq("/trips/dashboard/#{@trip_id}/checklist/#{@checklist_id}")
       expect(page).to_not have_field(:name, with: 'Cheez-its')
+    end
+
+    it 'can create new item with name as New Item' do
+      empty_checklist_show = File.read('spec/fixtures/empty_checklist_show.json')
+      new_item_fixture = File.read('spec/fixtures/new_checklist_item.json')
+      json_response = File.read('spec/fixtures/user/user_info.json')
+      
+      stub_request(:get, "https://travel-buddy-api.herokuapp.com/api/v1/users/3112").to_return(status: 200, body: json_response, headers: {})
+      user = UserFacade.current_user_info(3112)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+
+      stub_request(:post, "https://travel-buddy-api.herokuapp.com/api/v1/trips/8/checklists/2/checklist_items?name=New%20Item&user_id=3112").
+         to_return(status: 200)
+
+      stub_request(:get, "https://travel-buddy-api.herokuapp.com/api/v1/trips/#{@trip_id}/checklists/#{@checklist_id}").
+         to_return(status: 200, body: new_item_fixture)
+      
+      expect(page).to_not have_field(:name, with: 'New Item')
+      click_on "Create New Item"
+
+      expect(current_path).to eq("/trips/dashboard/#{@trip_id}/checklist/#{@checklist_id}")
+      expect(page).to have_field(:name, with: 'New Item')
     end
   end
 end
