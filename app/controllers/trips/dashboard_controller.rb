51264355a4fa0ac14619082842cdf721d4d6a053
Carina Sweet <carina.sweet06@gmail.com>
2021-09-22 21:28:34 -0600

    Add edit trip name

--- app/controllers/trips/dashboard_controller.rb
+++ app/controllers/trips/dashboard_controller.rb
@@ -3,7 +3,7 @@ class Trips::DashboardController < ApplicationController
     @trip_id = params[:id]
     @trip = TripFacade.trip_details(@trip_id)
   end
-  
+
   def create
     park_code = params[:park_code]
     user_id = session[:user_id]
@@ -11,8 +11,9 @@ class Trips::DashboardController < ApplicationController
     @trip = TripFacade.new_trip(park_code, park_name, user_id)
     redirect_to trips_dashboard_path(@trip.id)
   end
-  
+
   def update
+    # require "pry";binding.pry
     update_info = params
     TripFacade.update_trip(params[:id], update_info)
     redirect_to trips_dashboard_path(params[:id])
