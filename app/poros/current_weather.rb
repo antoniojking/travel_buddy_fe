cb0bd3b06445342227ccdfee708518dcc0d2b72b
Elliot Olbright <elliotolbright@gmail.com>
2021-09-23 13:35:38 -0600

    tested new weather service

--- app/poros/current_weather.rb
+++ app/poros/current_weather.rb
@@ -1,10 +1,16 @@
 class CurrentWeather
   attr_reader :current_temp,
-              :current_conditions
+              :current_conditions,
+              :min_temp,
+              :max_temp,
+              :id
   
-
   def initialize(data)
-    @current_temp         = data[:current_temp]
-    @current_conditions   = data[:current_conditions]
+    @id                   = data[:id]
+    @current_temp         = data[:attributes][:current_temp]
+    @current_conditions   = data[:attributes][:current_conditions]
+    @min_temp             = data[:attributes][:min_temp]
+    @max_temp             = data[:attributes][:max_temp]
+
   end
 end
