b0337f0495fd63906c1ae4b23c6da845a2414259
Antonio King <antonio.j.king@gmail.com>
2021-09-23 22:18:46 -0600

    Refactor explore page code and clean up files

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -11,18 +11,5 @@ class ExploreController < ApplicationController
     else
       @activity = NationalParkFacade.parks_by_activity(params[:activity])
     end
-
-
-    # conn = Faraday.new(url: 'https://travel-buddy-api.herokuapp.com/api/v1/')
-
-    # response = conn.get('parks') do |req|
-    #   req.params[:activity] = params[:activity]
-    # end
-
-    # json = JSON.parse(response.body, symbolize_names: true)
-    # @activity = json[:data]
-  end
-
-  def show
   end
 end
